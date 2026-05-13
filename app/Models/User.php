8e5d3691ff308760b325d2d7527393cefdc70c93
juki3r <deepong25@gmail.com>
2026-05-13 15:48:29 +0800

    aaa

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'name',
         'phone',
         'password',
+        'role',
+        'phone_verified',
+        'otp',
+        'granted'
     ];
 
     /**
