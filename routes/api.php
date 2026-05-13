792ebba7091203de76cf49102f5ebe76183ce1bd
juki3r <deepong25@gmail.com>
2026-05-13 15:10:30 +0800

    aa

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,14 @@ use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/ping', function () {
+    return response()->json([
+        'success' => true,
+        'message' => 'API is working'
+    ], 200);
+});
+
+
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
