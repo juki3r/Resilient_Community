1128fa7cd2031c928efa88ce37771c0311df84eb
juki3r <deepong25@gmail.com>
2026-05-25 14:25:09 +0800

    aa

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -151,7 +151,7 @@ class NewsController extends Controller
             ->get();
 
         return response()->json([
-            'unread_news' => $news,
+            'notifications' => $news,
             'unread_count' => $news->count()
         ]);
     }
