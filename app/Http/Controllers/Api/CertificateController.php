6ff80442ce5183e1ad678aa8d91fef13d34e4e5b
juki3r <deepong25@gmail.com>
2026-05-27 09:27:59 +0800

    aa

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -236,6 +236,8 @@ class CertificateController extends Controller
                     $title,
                     $body,
                     [
+                        'screen' => 'Requests',
+                        'request_id' => (string) $documentRequest->id,
                         'url' => 'https://google.com',
                     ]
                 );
