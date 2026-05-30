bf48e968f4db5c7dd69f26c7bead619a2b42576e
juki3r <deepong25@gmail.com>
2026-05-30 09:47:17 +0800

    aa

--- config/cors.php
+++ config/cors.php
@@ -1,26 +1,5 @@
 <?php
 
-// return [
-
-//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-//     'allowed_methods' => ['*'],
-
-//     'allowed_origins' => [
-//         'https://ajcpisonet.com',
-//         'http://localhost:5173',
-//     ],
-
-//     'allowed_origins_patterns' => [],
-
-//     'allowed_headers' => ['*'],
-
-//     'exposed_headers' => [],
-
-//     'max_age' => 0,
-
-//     'supports_credentials' => true,
-// ];
 
 return [
 
@@ -32,7 +11,7 @@ return [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
         'https://ajcpisonet.com',
-        'https://mdrrmo-web-five.vercel.app/',
+        'https://mdrrmo-web-five.vercel.app',
     ],
 
     'allowed_origins_patterns' => [],
